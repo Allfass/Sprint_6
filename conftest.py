301c42da6d011d34e341391636ac3f94cb79acf4
Alf <sh4mil.nasyrov@gmail.com>
2024-06-12 15:06:52 +0500

    run two browser and headless mode

--- conftest.py
+++ conftest.py
@@ -4,13 +4,28 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver import ChromeOptions
 
-@pytest.fixture(params=["firefox"])
+def pytest_addoption(parser):
+    parser.addoption("--headless", action="store", default="no")
+
+@pytest.fixture(params=["firefox","chrome"])
 def driver(request):
-    drivers = {
-        #"chrome": webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())),
-        "firefox": webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-    }
-    request.cls.driver = drivers[request.param]
+    if request.config.getoption('--headless') == 'yes':
+        firefox_opts = FirefoxOptions()
+        chrome_opts = ChromeOptions()
+        firefox_opts.add_argument("--headless")
+        chrome_opts.add_argument("--headless")
+        if request.param == 'firefox':
+            browser = webdriver.Firefox(options=firefox_opts)
+        else:
+            browser = webdriver.Chrome(options=chrome_opts)
+    else:
+        if request.param == 'firefox':
+            browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        else:
+            browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    request.cls.driver = browser
     yield request.cls.driver
     request.cls.driver.quit()
